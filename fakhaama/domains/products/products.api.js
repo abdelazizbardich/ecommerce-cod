e3f9e0e4301128846c6f737c6dfc4b576b2cb86a
root <abdelazizbardich@gmail.com>
2023-06-03 19:25:58 +0000

    adding products

--- fakhaama/domains/products/products.api.js
+++ fakhaama/domains/products/products.api.js
@@ -4,6 +4,7 @@ const productsService = require('./products.service');
 
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
+    console.log('id',id);
     const product = await productsService.getProduct(id);
     const linkedProducts = await productsService.getProductsByCategory(product.category_id);
     const reviews = await productsService.getReviwes(id);
